4108e978b5f04086c87dee11fa6ad734bcbf5aa1
madaagnol <maicon.dallagnol@outlook.com>
2017-05-25 00:45:18 -0300

    Att

--- ProjetoED1/lista.h
+++ ProjetoED1/lista.h
@@ -6,19 +6,19 @@ typedef struct lista Lista;
 /* funcao de criao: retorna uma lista vazia */
 Lista* lst_cria();
 
-/* inser��o no in�cio: retorna a lista atualizada */
+/* inserir no inicio: retorna a lista atualizada */
 Lista* lst_insere(Lista* l, int i);
 
-/* fun��o imprime: imprime valores dos elementos */
+/* Função imprime: imprime valores dos elementos */
 void lst_imprime(Lista* l);
 
-/* fun��o vazia: retorna 1 se vazia ou 0 se nao vazia */
+/* Função vazia: retorna 1 se vazia ou 0 se nao vazia */
 int lst_vazia(Lista* l);
 
-/* fun��o busca: busca um elemento na lista */
+/* Função busca: busca um elemento na lista */
 Lista* busca(Lista* l, int v);
 
-/* fun��o retira: retira elemento da lista */
+/* Função retira: retira elemento da lista */
 Lista* lst_retira(Lista* l, int v);
 
 /*Libera a lista completa*/
